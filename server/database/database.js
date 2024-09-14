2ebfc13d36b217686f92060203b9347193c3dc78
JuanManuelRamosL <juanmanuelramoslencina@gmail.com>
2024-09-14 13:22:47 -0300

    Prueba

--- server/database/database.js
+++ server/database/database.js
@@ -1,4 +1,4 @@
-// Importa Sequelize y dotenv
+/* // Importa Sequelize y dotenv
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 
@@ -18,3 +18,29 @@ export const sequelize = new Sequelize(
 );
 
 
+ */
+
+// Importa Sequelize y dotenv
+import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
+
+// Carga las variables de entorno
+dotenv.config();
+
+// Crea la instancia de Sequelize usando las variables de entorno
+export const sequelize = new Sequelize(
+  process.env.POSTGRES_DATABASE,        // Nombre de la base de datos
+  process.env.POSTGRES_USER,        // Usuario
+  process.env.POSTGRES_PASSWORD,    // Contraseña
+  {
+    host: process.env.POSTGRES_HOST.split(':')[0], // Host (sin el puerto)
+    port: process.env.POSTGRES_HOST.split(':')[1], // Puerto
+    dialect: 'postgres',                     // Dialecto que estás usando
+    dialectOptions: {
+      ssl: {
+        require: true,                      // Requiere SSL
+        rejectUnauthorized: false           // Evitar el rechazo de certificados no autorizados
+      }
+    }
+  }
+);
