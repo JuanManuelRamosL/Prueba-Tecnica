f4b6ed21a2ca96db33b107f926ed6cac4fa4eb43
JuanManuelRamosL <juanmanuelramoslencina@gmail.com>
2024-09-14 16:14:39 -0300

    Estilos cards productos

--- front/src/components/nav.jsx
+++ front/src/components/nav.jsx
@@ -1,9 +1,13 @@
 // front/components/Nav.jsx
 import Link from "next/link";
+import "./nav.css";
 
 export default function Nav() {
   return (
     <nav className="bg-gray-800 p-4">
+      <div className="container-buscador">
+        <input type="text" />
+      </div>
       <ul className="flex space-x-4">
         <li>
           <Link href="/home">
@@ -12,9 +16,7 @@ export default function Nav() {
         </li>
         <li>
           <Link href="/administrar">
-            <p className="text-white hover:text-gray-400">
-              Modificar Productos
-            </p>
+            <p className="text-white hover:text-gray-400">Mis Productos</p>
           </Link>
         </li>
       </ul>
