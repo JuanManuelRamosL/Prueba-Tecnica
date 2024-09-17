594b20ecd745cabdeb3328cc147fb63702f880a0
juanmanuel <juanmanuelramoslencina@gmail.com>
2024-09-17 10:45:22 -0300

    validaciones en crear product front

--- front/src/app/crear/page.jsx
+++ front/src/app/crear/page.jsx
@@ -1,4 +1,4 @@
-"use client"; // Si estás usando Next.js con el app directory
+"use client";
 
 import React, { useState, useEffect } from "react";
 import useProductStore from "@/store/useProductStore";
@@ -26,9 +26,36 @@ export default function CreateProductForm() {
     });
   };
 
+  // Función para validar los datos del formulario
+  const validateForm = () => {
+    if (!formData.name) {
+      return "El nombre del producto es obligatorio.";
+    }
+    if (formData.description.length > 250) {
+      return "La descripción es demasiado larga. Debe tener menos de 250 caracteres.";
+    }
+    if (!formData.description) {
+      return "La descripción es requerida.";
+    }
+    if (!formData.image) {
+      return "La imagen del producto es obligatoria.";
+    }
+    if (formData.price <= 0) {
+      return "El precio debe ser un valor positivo.";
+    }
+    return null; // No hay errores
+  };
+
   // Función que se ejecuta al enviar el formulario
   const handleSubmit = async (e) => {
     e.preventDefault();
+    const validationError = validateForm();
+    if (validationError) {
+      setError(validationError);
+      setSuccessMessage(""); // Limpiar el mensaje de éxito si hay error
+      return;
+    }
+
     const newProduct = await createProduct(formData);
     if (!newProduct) {
       setError("Hubo un problema al crear el producto.");
@@ -43,13 +70,15 @@ export default function CreateProductForm() {
   useEffect(() => {
     fetchProducts();
     setLista(true);
-  }, []);
+  }, [fetchProducts]);
 
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-2xl font-bold mb-4">Crear Producto</h1>
 
+      {/* Mostrar mensaje de error */}
       {error && <div className="text-red-500">{error}</div>}
+      {/* Mostrar mensaje de éxito */}
       {successMessage && (
         <div
           className="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative"
@@ -58,7 +87,6 @@ export default function CreateProductForm() {
           <span className="block sm:inline">{successMessage}</span>
         </div>
       )}
-      {/* Mostrar mensaje de éxito */}
 
       <form onSubmit={handleSubmit} className="space-y-4">
         <div>
